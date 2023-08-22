f0ec8e39d8921d3a369607e2fa0045d086ad2903
dzimiks <vana997@gmail.com>
2023-08-22 23:46:02 +0200

    feat: update snap manifest permissions & add Tenderly branding

--- packages/site/src/pages/index.tsx
+++ packages/site/src/pages/index.tsx
@@ -129,7 +129,7 @@ const Index = () => {
   return (
     <Container>
       <Heading>
-        Welcome to <Span>template-snap</Span>
+        Welcome to <Span>Tenderly Snap</Span>
       </Heading>
       <Subtitle>
         Get started by editing <code>src/index.ts</code>
